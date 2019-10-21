3649ed5913f26b764fb7a4ded8e9841498eeb132
InvisibleSpectator <vladislav5678@yandex.ru>
2019-10-22 01:24:21 +0400

    времявыбиралка
    работает, сбрасывает дату на 70 год, но время сохраняет. надо будет попробовать перейти на java.time

--- app/src/main/java/com/matmik/mapalarm/android/EditNoteActivity.kt
+++ app/src/main/java/com/matmik/mapalarm/android/EditNoteActivity.kt
@@ -3,11 +3,22 @@ package com.matmik.mapalarm.android
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.view.View
 import android.widget.*
 import com.matmik.mapalarm.android.db.DbHelper
 import com.matmik.mapalarm.android.model.Alarm
 import com.matmik.mapalarm.android.model.Options
+import java.text.SimpleDateFormat
+import android.widget.TimePicker
+import android.app.TimePickerDialog
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import java.util.*
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import java.text.DateFormat
 
 
 class EditNoteActivity : AppCompatActivity(),View.OnClickListener {
@@ -15,20 +26,65 @@ class EditNoteActivity : AppCompatActivity(),View.OnClickListener {
     private lateinit var alarm: Alarm
     private lateinit var dbHelper: DbHelper
 
+    private var mHour: Int = 0
+    private var mMinute: Int = 0
+
+
+    private lateinit var name: TextView
+    private lateinit var time :EditText
+
+    private lateinit var monday: CheckBox
+
+    private lateinit var tuesday: CheckBox
+
+    private lateinit var wednesday: CheckBox
+
+    private lateinit var thursday : CheckBox
+
+    private lateinit var friday: CheckBox
+
+    private lateinit var saturday: CheckBox
+
+    private lateinit var sunday: CheckBox
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_note)
+
+
+        name =findViewById(R.id.name)
+        time = findViewById(R.id.time)
+
+        monday = findViewById<CheckBox>(R.id.monday)
+
+        tuesday = findViewById<CheckBox>(R.id.tuesday)
+
+        wednesday = findViewById<CheckBox>(R.id.wednesday)
+
+        thursday = findViewById<CheckBox>(R.id.thursday)
+
+        friday = findViewById<CheckBox>(R.id.friday)
+
+        saturday = findViewById<CheckBox>(R.id.saturday)
+
+        sunday = findViewById<CheckBox>(R.id.sunday)
+
+
+
+
         dbHelper = DbHelper(this)
         alarm = intent.getSerializableExtra("EditableNote") as Alarm
-        findViewById<TextView>(R.id.name).text = alarm.name
+        name.text = alarm.name
+        time.setOnClickListener(this)
+        time.setText(SimpleDateFormat("HH : mm").format(alarm.time))
 
-        findViewById<CheckBox>(R.id.monday).isChecked = alarm.options.contains(Options.Monday)
-        findViewById<CheckBox>(R.id.tuesday).isChecked = alarm.options.contains(Options.Tuesday)
-        findViewById<CheckBox>(R.id.wednesday).isChecked = alarm.options.contains(Options.Wednesday)
-        findViewById<CheckBox>(R.id.thursday).isChecked = alarm.options.contains(Options.Thursday)
-        findViewById<CheckBox>(R.id.friday).isChecked = alarm.options.contains(Options.Friday)
-        findViewById<CheckBox>(R.id.saturday).isChecked = alarm.options.contains(Options.Saturday)
-        findViewById<CheckBox>(R.id.sunday).isChecked = alarm.options.contains(Options.Sunday)
+        monday.isChecked = alarm.options.contains(Options.Monday)
+        tuesday.isChecked = alarm.options.contains(Options.Tuesday)
+        wednesday.isChecked = alarm.options.contains(Options.Wednesday)
+        thursday.isChecked = alarm.options.contains(Options.Thursday)
+        friday.isChecked = alarm.options.contains(Options.Friday)
+        saturday.isChecked = alarm.options.contains(Options.Saturday)
+        sunday.isChecked = alarm.options.contains(Options.Sunday)
 
         findViewById<Switch>(R.id.active).isChecked = alarm.active
         findViewById<Switch>(R.id.located).isChecked = alarm.locationBound
@@ -38,22 +94,48 @@ class EditNoteActivity : AppCompatActivity(),View.OnClickListener {
         when (p0?.id) {
 
             R.id.save -> {
-                alarm.name = findViewById<TextView>(R.id.name).text.toString()
+                alarm.name = name.text.toString()
                 alarm.options.clear()
-                if (findViewById<CheckBox>(R.id.monday).isChecked) alarm.options.add(Options.Monday)
-                if (findViewById<CheckBox>(R.id.tuesday).isChecked) alarm.options.add(Options.Tuesday)
-                if (findViewById<CheckBox>(R.id.wednesday).isChecked) alarm.options.add(Options.Wednesday)
-                if (findViewById<CheckBox>(R.id.thursday).isChecked) alarm.options.add(Options.Thursday)
-                if (findViewById<CheckBox>(R.id.friday).isChecked) alarm.options.add(Options.Friday)
-                if (findViewById<CheckBox>(R.id.saturday).isChecked) alarm.options.add(Options.Saturday)
-                if (findViewById<CheckBox>(R.id.sunday).isChecked) alarm.options.add(Options.Sunday)
+                if (monday.isChecked) alarm.options.add(Options.Monday)
+                if (tuesday.isChecked) alarm.options.add(Options.Tuesday)
+                if (wednesday.isChecked) alarm.options.add(Options.Wednesday)
+                if (thursday.isChecked) alarm.options.add(Options.Thursday)
+                if (friday.isChecked) alarm.options.add(Options.Friday)
+                if (saturday.isChecked) alarm.options.add(Options.Saturday)
+                if (sunday.isChecked) alarm.options.add(Options.Sunday)
+
+
+                alarm.time=SimpleDateFormat("HH : mm").parse(time.text.toString())
+
 
                 alarm.active = findViewById<Switch>(R.id.active).isChecked
                 alarm.locationBound = findViewById<Switch>(R.id.located).isChecked
+                dbHelper.updateAlarm(alarm)
+                val editIntent= Intent(this,MainActivity::class.java)
+                startActivity(editIntent)
+            }
+            R.id.time ->{
+                callTimePicker()
             }
         }
-        dbHelper.updateAlarm(alarm)
-        val editIntent= Intent(this,MainActivity::class.java)
-        startActivity(editIntent)
+
     }
+
+
+    private fun callTimePicker() {
+        // получаем текущее время
+        val cal = Calendar.getInstance()
+        mHour = cal.get(Calendar.HOUR_OF_DAY)
+        mMinute = cal.get(Calendar.MINUTE)
+
+        // инициализируем диалог выбора времени текущими значениями
+        val timePickerDialog = TimePickerDialog(this,
+            TimePickerDialog.OnTimeSetListener { _, hourOfDay, minute ->
+                val editTextTimeParam = "$hourOfDay : $minute"
+                time.setText(editTextTimeParam)
+            }, mHour, mMinute, true
+        )
+        timePickerDialog.show()
+    }
+
 }
