84544cc108e35a6921619e94a4f83152f81103f6
InvisibleSpectator <vladislav5678@yandex.ru>
2019-11-26 23:22:52 +0400

    Тост

--- app/src/main/java/com/matmik/mapalarm/android/EditNoteActivity.kt
+++ app/src/main/java/com/matmik/mapalarm/android/EditNoteActivity.kt
@@ -1,6 +1,7 @@
 package com.matmik.mapalarm.android
 
 import android.app.TimePickerDialog
+import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
@@ -19,7 +20,7 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 
-class EditNoteActivity : AppCompatActivity(),View.OnClickListener {
+class EditNoteActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var alarm: Alarm
     private lateinit var dbHelper: DbHelper
@@ -29,7 +30,7 @@ class EditNoteActivity : AppCompatActivity(),View.OnClickListener {
 
 
     private lateinit var name: EditText
-    private lateinit var time :EditText
+    private lateinit var time: EditText
 
     private lateinit var monday: CheckBox
 
@@ -37,7 +38,7 @@ class EditNoteActivity : AppCompatActivity(),View.OnClickListener {
 
     private lateinit var wednesday: CheckBox
 
-    private lateinit var thursday : CheckBox
+    private lateinit var thursday: CheckBox
 
     private lateinit var friday: CheckBox
 
@@ -52,7 +53,7 @@ class EditNoteActivity : AppCompatActivity(),View.OnClickListener {
         setContentView(R.layout.activity_edit_note)
 
 
-        name =findViewById(R.id.name)
+        name = findViewById(R.id.name)
         time = findViewById(R.id.time)
 
         monday = findViewById<CheckBox>(R.id.monday)
@@ -69,17 +70,21 @@ class EditNoteActivity : AppCompatActivity(),View.OnClickListener {
 
         sunday = findViewById<CheckBox>(R.id.sunday)
 
-        description=findViewById(R.id.description)
+        description = findViewById(R.id.description)
 
-        findViewById<TextView>(R.id.location).isClickable=findViewById<Switch>(R.id.located).isChecked
-        if(!findViewById<Switch>(R.id.located).isChecked) findViewById<TextView>(R.id.location).setTextColor(
-            Color.GRAY) else findViewById<TextView>(R.id.location).setTextColor(Color.BLACK)
+        findViewById<TextView>(R.id.location).isClickable =
+            findViewById<Switch>(R.id.located).isChecked
+        if (!findViewById<Switch>(R.id.located).isChecked) findViewById<TextView>(R.id.location).setTextColor(
+            Color.GRAY
+        ) else findViewById<TextView>(R.id.location).setTextColor(Color.BLACK)
 
 
         findViewById<Switch>(R.id.located).setOnCheckedChangeListener { compoundButton: CompoundButton, b: Boolean ->
-            findViewById<TextView>(R.id.location).isClickable=findViewById<Switch>(R.id.located).isChecked
-            if(!findViewById<Switch>(R.id.located).isChecked) findViewById<TextView>(R.id.location).setTextColor(
-                Color.GRAY) else findViewById<TextView>(R.id.location).setTextColor(Color.BLACK)
+            findViewById<TextView>(R.id.location).isClickable =
+                findViewById<Switch>(R.id.located).isChecked
+            if (!findViewById<Switch>(R.id.located).isChecked) findViewById<TextView>(R.id.location).setTextColor(
+                Color.GRAY
+            ) else findViewById<TextView>(R.id.location).setTextColor(Color.BLACK)
         }
 
         dbHelper = DbHelper(this)
@@ -108,42 +113,53 @@ class EditNoteActivity : AppCompatActivity(),View.OnClickListener {
         when (p0?.id) {
 
             R.id.save -> {
-                alarm.name = name.text.toString()
-                alarm.options.clear()
-                if (monday.isChecked) alarm.options.add(Options.Monday)
-                if (tuesday.isChecked) alarm.options.add(Options.Tuesday)
-                if (wednesday.isChecked) alarm.options.add(Options.Wednesday)
-                if (thursday.isChecked) alarm.options.add(Options.Thursday)
-                if (friday.isChecked) alarm.options.add(Options.Friday)
-                if (saturday.isChecked) alarm.options.add(Options.Saturday)
-                if (sunday.isChecked) alarm.options.add(Options.Sunday)
+                if (findViewById<Switch>(R.id.located).isChecked && findViewById<TextView>(R.id.location).text == "") {
+                    Toast.makeText(applicationContext, R.string.coord_err, Toast.LENGTH_SHORT)
+                        .show()
+                } else {
+                    alarm.name = name.text.toString()
+                    alarm.options.clear()
+                    if (monday.isChecked) alarm.options.add(Options.Monday)
+                    if (tuesday.isChecked) alarm.options.add(Options.Tuesday)
+                    if (wednesday.isChecked) alarm.options.add(Options.Wednesday)
+                    if (thursday.isChecked) alarm.options.add(Options.Thursday)
+                    if (friday.isChecked) alarm.options.add(Options.Friday)
+                    if (saturday.isChecked) alarm.options.add(Options.Saturday)
+                    if (sunday.isChecked) alarm.options.add(Options.Sunday)
 
 
-                alarm.time = SimpleDateFormat("HH : mm").parse(time.text.toString())
+                    alarm.time = SimpleDateFormat("HH : mm").parse(time.text.toString())
 
 
 
-                alarm.active = findViewById<Switch>(R.id.active).isChecked
-                alarm.locationBound = findViewById<Switch>(R.id.located).isChecked
+                    alarm.active = findViewById<Switch>(R.id.active).isChecked
+                    alarm.locationBound = findViewById<Switch>(R.id.located).isChecked
 
-                alarm.description = description.text.toString()
-                if (alarm.id >= 0)
+                    alarm.description = description.text.toString()
+                    if (alarm.id >= 0)
                     //dbHelper.updateAlarm(alarm)
-                    MapAlarmManager.updateAndSchedule(this.applicationContext, alarm, dbHelper)
-                else {
-                    //dbHelper.addAlarm(alarm)
-                    alarm = MapAlarmManager.addAndSchedule(this.applicationContext, alarm, dbHelper)
+                        MapAlarmManager.updateAndSchedule(this.applicationContext, alarm, dbHelper)
+                    else {
+                        //dbHelper.addAlarm(alarm)
+                        alarm =
+                            MapAlarmManager.addAndSchedule(this.applicationContext, alarm, dbHelper)
+                    }
+                    if (findViewById<Switch>(R.id.located).isChecked) {
+                        val latid = alarm.location.split(",")[0].toDouble()
+                        val longt = alarm.location.split(",")[1].toDouble()
+                    } else {
+                        
+                    }
+
+                    //GlobalVariables.items.add(OverlayItem(alarm.id.toString(), alarm.name, alarm.description, GeoPoint(latid, longt)))
+                    val editIntent = Intent(this, MainActivity::class.java)
+                    startActivity(editIntent)
                 }
-                val latid = alarm.location.split(",")[0].toDouble()
-                val longt = alarm.location.split(",")[1].toDouble()
-                //GlobalVariables.items.add(OverlayItem(alarm.id.toString(), alarm.name, alarm.description, GeoPoint(latid, longt)))
-                val editIntent = Intent(this, MainActivity::class.java)
-                startActivity(editIntent)
             }
             R.id.time -> {
                 callTimePicker()
             }
-            R.id.location ->{
+            R.id.location -> {
                 println("ЭЭЭЭ")
             }
         }
@@ -158,7 +174,8 @@ class EditNoteActivity : AppCompatActivity(),View.OnClickListener {
         mMinute = cal.get(Calendar.MINUTE)
 
         // инициализируем диалог выбора времени текущими значениями
-        val timePickerDialog = TimePickerDialog(this,
+        val timePickerDialog = TimePickerDialog(
+            this,
             TimePickerDialog.OnTimeSetListener { _, hourOfDay, minute ->
                 val editTextTimeParam = "$hourOfDay : $minute"
                 time.setText(editTextTimeParam)
