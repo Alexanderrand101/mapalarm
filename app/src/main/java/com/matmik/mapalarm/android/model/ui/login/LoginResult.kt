1292607ae9632748931fa983a0fa32f840e72563
Alexanderrand101 <Sonicrider2@yandex.ru>
2019-11-26 17:46:54 +0400

    Recovery

--- app/src/main/java/com/matmik/mapalarm/android/model/ui/login/LoginResult.kt
+++
@@ -1,9 +0,0 @@
-package com.matmik.mapalarm.android.model.ui.login
-
-/**
- * Authentication result : success (user details) or error message.
- */
-data class LoginResult(
-    val success: LoggedInUserView? = null,
-    val error: Int? = null
-)
